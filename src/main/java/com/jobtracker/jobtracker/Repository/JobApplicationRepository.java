bc95efd8d9cbfc37b84b1a5a57685aa6d2bee9b9
04hunterdunn <04hunterdunn@gmail.com>
2025-10-27 13:31:50 -0500

    Delete src directory

--- src/main/java/com/jobtracker/jobtracker/Repository/JobApplicationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jobtracker.jobtracker.Repository;
-
-import com.jobtracker.jobtracker.model.JobApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobApplicationRepository extends JpaRepository<JobApplication, Long>{
-
-}
